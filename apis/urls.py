64cf7a6eb63f41ce54c3312aee8db0107f41f05d
mark-akbarov <mark-akbarov@gmail.com>
2022-01-04 19:43:57 +0500

    refactoring DRF

--- apis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import ListArticle, DetailArticle
-
-urlpatterns = [
-    path('', ListArticle.as_view()),
-    path('<int:pk>/', DetailArticle.as_view()),
-]
--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('articles/', include('articles.urls')),
-    path('apis/v1/', include('apis.urls')),
+    path('apis/v1/', include('articles.urls')),
     path('', include('pages.urls')),
 ]
